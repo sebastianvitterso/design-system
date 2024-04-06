337de761b73cbfbd4a31c8cda7647d475cf52ced
Victor Nystad <vnys@equinor.com>
2024-04-06 15:51:49 +0000

    🚧  Run storefront in Docker

--- apps/storefront/next.config.mjs
+++ apps/storefront/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: 'cdn.sanity.io' }] },
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: '../../',
+  },
 }
 
 export default nextConfig
